1c51c08c5802bebe3d41e78adc18cc3d8a6792de
takumikamimae-tech <takumi.kamimae@neo-career.co.jp>
2025-12-15 11:21:06 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000, // 開発サーバーのポートを3000に固定（お好みで変更可）
+    open: true, // サーバー起動時に自動でブラウザを開く
+  },
+})
