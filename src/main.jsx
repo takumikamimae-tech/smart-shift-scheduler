08f6cd10b54a50a917946dfee765560bb4144df7
takumikamimae-tech <takumi.kamimae@neo-career.co.jp>
2025-12-15 11:28:38 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // 追加
 import App from './App.jsx';
-import './index.css'; // Tailwind CSSを読み込むために必須
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> {/* 追加 */}
+      <App />
+    </BrowserRouter> {/* 追加 */}
   </React.StrictMode>,
 );
